2d59784eaa66c077b7fe078e6009d552ae3ee0e2
ArkKnight271 <2heartsdown@gmail.com>
2015-12-11 16:54:19 -0600

    initial commit

---
+++ Helloworld/Helloworld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld
+{
+    class Helloworld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!, this is some extra descriptive text.");
+            Console.ReadKey();
+
+        }
+    }
+}

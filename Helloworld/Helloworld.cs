589fcc737cfc10585a9deeeaf675c1979b0398b2
ArkKnight271 <2heartsdown@gmail.com>
2015-12-11 16:56:43 -0600

    Removed Some text.

--- Helloworld/Helloworld.cs
+++ Helloworld/Helloworld.cs
@@ -10,7 +10,7 @@ namespace Helloworld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!, this is some extra descriptive text.");
+            Console.WriteLine("Hello World!");
             Console.ReadKey();
 
         }
